a45f25ce074e07b4f19f1ef2927c39eab30236a7
Kevin Douglas <121463119+cybergrizz@users.noreply.github.com>
2023-04-23 20:21:16 -0400

    04/23/23 update to output files

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+#List of available availability zones in given region
+output "list_of_az" {
+  value = data.aws_availability_zones.available-azs[*].names
+}
+
+#List of public subnet IDs
+output "public_subnet_id" {
+  value = data.aws_subnets.public[*].ids
+}
+
+#List of private subnet IDs
+output "private_subnet_id" {
+  value = data.aws_subnets.private[*].ids
+}
+
+#DB instance address
+output "db-address" {
+    value = aws_db_instance.mysql.address
+}
