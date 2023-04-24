d2e9f4f0ada7193ba7896b5f3ca4c736051a47eb
Kevin Douglas <121463119+cybergrizz@users.noreply.github.com>
2023-04-23 21:58:33 -0400

    security group updates

--- sg.tf
+++ sg.tf
@@ -7,23 +7,24 @@ resource "aws_security_group" "lt-sg" {
   ingress {
     from_port       = var.http_port
     to_port         = var.http_port
-    protocol        = "tcp"
-    security_groups = [aws_security_group.alb-sg.id]
+    protocol        = var.tcp
+    security_groups = [aws_security_group.terraform-sg.id]
   }
 
   ingress {
     from_port       = var.ssh_port
     to_port         = var.ssh_port
-    protocol        = "tcp"
-    security_groups = [aws_security_group.alb-sg.id]
+    protocol        = var.tcp
+    security_groups = [aws_security_group.terraform-sg.id]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = var.egress-all
+    to_port     = var.egress-all
+    protocol    = var.egress
     cidr_blocks = [var.cidr]
   }
+
 
   tags = {
     Name = "lt-sg"
@@ -31,32 +32,36 @@ resource "aws_security_group" "lt-sg" {
 }
 
 #####SECURITY GROUP#####
-resource "aws_security_group" "alb-sg" {
-  name   = var.alb_sg_name
+resource "aws_security_group" "terraform-sg" {
+  name   = var.sg_name
   vpc_id = aws_vpc.terraform-vpc.id
 
   ingress {
     from_port   = var.http_port
     to_port     = var.http_port
-    protocol    = "tcp"
+    protocol    = var.tcp
     cidr_blocks = [var.cidr]
   }
 
   ingress {
     from_port   = var.ssh_port
     to_port     = var.ssh_port
-    protocol    = "tcp"
+    protocol    = var.tcp
     cidr_blocks = [var.cidr]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = var.egress-all
+    to_port     = var.egress-all
+    protocol    = var.egress
     cidr_blocks = [var.cidr]
+  }
+  
+  lifecycle {
+    create_before_destroy = true
   }
 
   tags = {
-    Name = "alb-sg"
+    Name = "terraform-sg"
   }
 }
