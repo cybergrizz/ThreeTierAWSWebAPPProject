e5df8aefcc2709edaaeb57d5600eba8c4c4d4e5c
Kevin Douglas <121463119+cybergrizz@users.noreply.github.com>
2023-04-24 20:29:09 -0400

    S3 updated

--- s3.tf
+++ s3.tf
@@ -1,14 +1,10 @@
 #####STATIC WEBSITE STORED HERE#####
 resource "aws_s3_bucket_website_configuration" "web-app" {
   bucket = var.bucket_name
-
-  tags = {
-    name = "Web App"
-  }
 }
 
-resource "aws_s3_bucket_acl" "bucker_acl" {
-  bucket = [aws_s3_bucket.web-app.id]
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = var.bucket_name
   acl    = "private"
 
 }
